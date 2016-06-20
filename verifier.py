59abfcaaf2d2398cc6379f9c0239382f53b13d60
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-06-21 00:57:50 +0800

    Intro GnipDataFile class to hold the data after verification. Display total data lines after all verification

--- verifier.py
+++ verifier.py
@@ -1,35 +1,40 @@
 import os, json, sys
 
 
-def is_json(line):
-    try:
-        json.loads(line)
-    except ValueError, e:
+class GnipDataFile:
+    @staticmethod
+    def is_json(line):
+        try:
+            json.loads(line)
+        except ValueError, e:
+            return False
+        return True
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.activity_count = 0
+
+    def is_valid(self):
+        valid_lines = 0
+        last_line = ''
+
+        with open(self.filename) as f:
+            for line in f:
+                if len(line.strip()) < 1:
+                    continue
+
+                last_line = line
+                if self.is_json(line):
+                    valid_lines += 1
+
+        json_checksum = json.loads(last_line)
+        if 'info' in json_checksum and 'activity_count' in json_checksum['info']:
+            self.activity_count = json_checksum['info']['activity_count']
+            expected_count = valid_lines - 1
+            if self.activity_count == expected_count:
+                return True
+
         return False
-    return True
-
-
-def valid_gnip_json_file(filename):
-    valid_lines = 0
-    last_line = ''
-
-    with open(filename) as f:
-        for line in f:
-            if len(line.strip()) < 1:
-                continue
-
-            last_line = line
-            if is_json(line):
-                valid_lines += 1
-
-    json_checksum = json.loads(last_line)
-    if 'info' in json_checksum and 'activity_count' in json_checksum['info']:
-        activity_count = json_checksum['info']['activity_count']
-        expected_count = valid_lines - 1
-        if activity_count == expected_count:
-            return True
-
-    return False
 
 if __name__ == '__main__':
     working_directory = os.getcwd()
@@ -42,13 +47,17 @@ if __name__ == '__main__':
         break
 
     verified = 0
+    data_lines = 0
     for index, f in enumerate(files):
-        if valid_gnip_json_file(os.path.join(json_directory, f)):
+        gnip_file = GnipDataFile(os.path.join(json_directory, f))
+        if gnip_file.is_valid():
             sys.stdout.write('.')
             sys.stdout.flush()
             verified += 1
+            data_lines += gnip_file.activity_count
         else:
             sys.stdout.write('[{}]'.format(index))
             sys.stdout.flush()
 
     print '{} of {} correct'.format(verified, len(files))
+    print '{} activity count'.format(data_lines)
